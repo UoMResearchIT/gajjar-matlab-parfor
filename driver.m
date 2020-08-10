53a855b3cab3739665119a7c2d3abe3127fbdb95
Gerard Capes <gerard.capes@manchester.ac.uk>
2020-08-10 14:01:39 +0100

    Add assertion to check new code gives correct answer

--- driver.m
+++ driver.m
@@ -6,3 +6,22 @@ ntw=tmax/tw;
 
 result = tpar(N,NZ,tmax,tw,ntw);
 disp(result)
+
+correct_result(:,:,1) = [
+     2     6    10;
+     3     7    11;
+     4     8    12;
+     5     9    13];
+ 
+correct_result(:,:,2) = [
+     3     7    11;
+     4     8    12;
+     5     9    13;
+     6    10    14];
+ 
+ correct_result(:,:,3) = [
+     4     8    12;
+     5     9    13;
+     6    10    14;
+     7    11    15];
+assert(isequal(result, correct_result))
