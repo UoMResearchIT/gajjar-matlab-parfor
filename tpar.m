6886cc363f25acfb63495887091ace4c1ebd21fb
Gerard Capes <gerard.capes@manchester.ac.uk>
2020-08-10 13:53:48 +0100

    Tidy formatting

--- tpar.m
+++ tpar.m
@@ -1,13 +1,13 @@
 function [D]= tpar(N,NZ,tmax,tw,ntw)
-    dt=.1
-    D=zeros(N,NZ,ntw);
+    dt = 0.1;
+    D = zeros(N,NZ,ntw);
     for kk=1:N
         ll=1:NZ;
         for jj=2:tmax
             t=jj*dt;
             jwrite=mod(jj,tw);
             njw=floor(jj/tw);
-            if(jwrite==0);
+            if(jwrite==0)
                 v(ll)=kk+(ll-1)*N+njw ;
                 D(kk,:,njw)=v(:);
             end
